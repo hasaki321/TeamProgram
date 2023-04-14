38d11ba60ac8050b24792e325bb32206c61f57de
320220940271-ci101 <320220940271@lzu.edu.cn>
2023-04-14 23:57:38 +0800

    forum login to be finished

---
+++ Teamprogram/app/src/main/java/com/example/teamprogram/ForumUserLoginPage.kt
@@ -0,0 +1,11 @@
+package com.example.teamprogram
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ForumUserLoginPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.forum_user_login_page)
+    }
+}
