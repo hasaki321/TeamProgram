aa61c9d641e61691b1a63ab2393669d19527afd7
320220940271-ci101 <320220940271@lzu.edu.cn>
2023-04-14 22:35:05 +0800

    froum database init

---
+++ Teamprogram/app/src/main/java/com/example/teamprogram/ui/forum/ForumDataBaseHelper.kt
@@ -0,0 +1,24 @@
+package com.example.teamprogram.ui.forum
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import android.widget.Toast
+
+class ForumDataBaseHelper(val context: Context, val name:String, val version: Int):
+    SQLiteOpenHelper(context,name,null,version) {
+
+    private val createLogin = "create table ${name} (" +
+            "id integer primary key," +
+            "username text," +
+            "email text)"
+
+    override fun onCreate(p0: SQLiteDatabase?) {
+        p0?.execSQL(createLogin)
+        Toast.makeText(context,"table created", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onUpgrade(p0: SQLiteDatabase?, p1: Int, p2: Int) {
+        TODO("Not yet implemented")
+    }
+}
