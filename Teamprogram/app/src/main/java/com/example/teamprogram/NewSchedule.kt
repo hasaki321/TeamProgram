282f17ba63edf56d5f2ac6af65dfb29279334ab0
320220940271-ci101 <320220940271@lzu.edu.cn>
2023-04-17 18:07:20 +0800

    homework data refresh finish

---
+++ Teamprogram/app/src/main/java/com/example/teamprogram/NewSchedule.kt
@@ -0,0 +1,4 @@
+package com.example.teamprogram
+
+class NewSchedule {
+}
--- Teamprogram/app/src/main/java/com/example/teamprogram/ui/dashboard/DashboardFragment.kt
+++ Teamprogram/app/src/main/java/com/example/teamprogram/ui/dashboard/DashboardFragment.kt
@@ -1,6 +1,12 @@
 package com.example.teamprogram.ui.dashboard
 
 import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -8,6 +14,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.teamprogram.databinding.FragmentDashboardBinding
@@ -15,70 +22,101 @@ import java.util.*
 import kotlin.collections.ArrayList
 
 @SuppressLint("Range")
-class DashboardFragment : Fragment() {
+class DashboardFragment : Fragment(),View.OnClickListener {
 
     private var _binding: FragmentDashboardBinding? = null
     private val binding get() = _binding!!
 
-    private val DataList = ArrayList<HomeWorkContent>()
+    private var DataList = ArrayList<HomeWorkContent>()
 
     lateinit var db:SQLiteDatabase
+    lateinit var cursor: Cursor
+    lateinit var timeChangeReceiver: TimeChangeReceiver
+    lateinit var broadcastManager:LocalBroadcastManager
+    lateinit var adapter: HomeworkAdapter
+    lateinit var recyclerView: RecyclerView
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val dashboardViewModel =
-            ViewModelProvider(this).get(DashboardViewModel::class.java)
-
         _binding = FragmentDashboardBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-
         db = HomeworkDataHelper(context!!,"homeworklist",1).readableDatabase
-        val cursor = db.query("homeworkList", null,null, null, null, null, "day ASC,hour ASC,minute ASC", null)
-        val calendar = java.util.Calendar.getInstance()
-        calendar.setTimeZone(TimeZone.getTimeZone("GMT+8:00"))
+        cursor = db.query("homeworkList", null,null, null, null, null, "day ASC,hour ASC,minute ASC", null)
 
-        //从数据库中取数据并判断剩余时间
-        run{
-            val _day = calendar.get(java.util.Calendar.DATE).toInt()
-            val _month = calendar.get(java.util.Calendar.MONTH).toInt() + 1
-            val _hour = calendar.get(java.util.Calendar.HOUR_OF_DAY).toInt() + 1
-            if (cursor.moveToFirst()) {
-                do {
-                    val title = cursor.getString(cursor.getColumnIndex("title"))
-                    val content = cursor.getString(cursor.getColumnIndex("content"))
-                    val month = cursor.getInt(cursor.getColumnIndex("month"))
-                    val day = cursor.getInt(cursor.getColumnIndex("day"))
-                    val hour = cursor.getInt(cursor.getColumnIndex("hour"))
-                    val minute = cursor.getInt(cursor.getColumnIndex("minute"))
+        //注册广播监听时间
+        broadcastManager = getActivity()?.let { LocalBroadcastManager.getInstance(it) }!!
+        val intentFilter = IntentFilter()
+        intentFilter.addAction("android.intent.action.TIME_TICK")
+        timeChangeReceiver = TimeChangeReceiver()
+        context!!.registerReceiver(timeChangeReceiver, intentFilter)
 
-                    var timeleft = 0
-
-                    if (month == _month) {
-                        val day_dif = day - _day
-                        val hour_dif = hour - _hour
-                        timeleft = day_dif * 24 + hour_dif
-                    }
-                    DataList.add(HomeWorkContent(title,content, timeleft.toString(),minute))
-                } while (cursor.moveToNext())
-            }
-        }
-
-        val recyclerView: RecyclerView = binding.recyclerviewDashboard
-
-        val homeworkList = DataList
-
-        recyclerView.adapter = HomeworkAdapter(homeworkList)
+        //创建作业列表
+        getList()
+        adapter = HomeworkAdapter(DataList)
+        recyclerView = binding.recyclerviewDashboard
+        recyclerView.adapter = adapter
         recyclerView.layoutManager = LinearLayoutManager(context)
 
         return root
     }
 
+    private fun getList(){
+        DataList = ArrayList<HomeWorkContent>()
+        val calendar = java.util.Calendar.getInstance()
+        calendar.setTimeZone(TimeZone.getTimeZone("GMT+8:00"))
+        val _day = calendar.get(java.util.Calendar.DATE).toInt()
+        val _month = calendar.get(java.util.Calendar.MONTH).toInt() + 1
+        val _hour = calendar.get(java.util.Calendar.HOUR_OF_DAY).toInt() + 1
+        val _minute = calendar.get(java.util.Calendar.MINUTE).toInt()
+        if (cursor.moveToFirst()) {
+            do {
+                val title = cursor.getString(cursor.getColumnIndex("title"))
+                val content = cursor.getString(cursor.getColumnIndex("content"))
+                val month = cursor.getInt(cursor.getColumnIndex("month"))
+                val day = cursor.getInt(cursor.getColumnIndex("day"))
+                val hour = cursor.getInt(cursor.getColumnIndex("hour"))
+                val minute = cursor.getInt(cursor.getColumnIndex("minute"))
+
+                var timeleft = 0
+
+                if (month == _month) {
+                    val day_dif = day - _day
+                    val hour_dif = hour - _hour
+                    val min_dif = minute - _minute
+                    timeleft = day_dif * 24 * 60 + hour_dif * 60 + min_dif
+                }
+                DataList.add(HomeWorkContent(title,content, (timeleft/60).toString(),timeleft%60))
+            } while (cursor.moveToNext())
+        }
+
+
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+        broadcastManager.unregisterReceiver(timeChangeReceiver)
+    }
+
+    inner class TimeChangeReceiver:BroadcastReceiver(){
+        @SuppressLint("NotifyDataSetChanged")
+        override fun onReceive(p0: Context?, p1: Intent?) {
+            getList()
+            adapter = HomeworkAdapter(DataList)
+            recyclerView.adapter = adapter
+            recyclerView.layoutManager = LinearLayoutManager(context)
+        }
+    }
+
+    override fun onClick(p0: View?) {
+        when(p0){
+            binding.addNewSchedule -> {
+
+            }
+        }
     }
 }
